44b062fcfed6c1d26321bdc239c8f5fdbb3ab5dd
Paul Seal <prjseal@gmail.com>
2019-09-06 00:13:04 +0100

    - added v8 website and set up component and composer

---
+++ SmallerImages/Compose/SmallerImagesComposer.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace SmallerImages.Compose
+{
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    public class SmallerImagesComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<SmallerImagesComponent>();
+        }
+    }
+}
